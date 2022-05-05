f029a96ed0d9bf902a690d4f52d227389cf0eac6
renancodepro <alvesrenan964@gmail.com>
2022-05-05 20:51:10 -0300

    python

---
+++ AnaliseBonusViagem.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+# que temos que baixar com pip install
+
+# pandas - integração do python com excel
+# openpyxl - mesmo de cima
+# twilio - integração do python com sms
+
+# passo a passo de solução
+
+# abrir os seis arquivos em Excel
+lista_meses =['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho']
+
+tabela_vendas = pd.read
+
+# Para cada arquivo:
+
+# verificar se algum valor na coluna vendas daquele arquivo é maior que 55.000
+
+# se for maior do que 55.000 -> Envia um sms com o nome, mês e as vendas do
+# vendedor
+
+# caso não seja maior que 55.000 não quero fazer nada
